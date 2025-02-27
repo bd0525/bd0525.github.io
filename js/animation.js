4e18921597bbf3a0a9ff083089a54e5a3f7450b4
byd <boyuandeng01@gmail.com>
2025-02-26 19:23:16 -0500

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const welcomeText = document.getElementById('welcomeText');
     
     // Text content for typewriter effect
-    const helloContent = "Hello, ___";
+    const helloContent = "Hello ,  ____";
     const welcomeContent = "Welcome to Boyuan's Homepage";
     
     // Typewriter effect function
